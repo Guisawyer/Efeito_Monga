f9ec265989643a37eb959f423ef8c46eff25091e
Guilherme Maia Santos <guisawyer@yahoo.com.br>
2026-05-28 12:56:19 -0300

    Update Potenciometro.ino

--- Potenciometro.ino
+++ Potenciometro.ino
@@ -1,15 +1,22 @@
-#define led1 9   
+#define led1 10   
 #define led2 11  
+
+#define led3 6   
+#define led4 5 
 
 void setup() {
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
 }
 
 void loop() {
-  int valor = analogRead(A0);               
-  int cor1  = map(valor, 0, 1023, 0, 255);   
-  int cor2  = map(valor, 0, 1023, 255, 0);   
+  int valor = analogRead(A0);                 
+  int cor1  = map(valor, 0, 1023, 0, 255);
+  int cor2  = map(valor, 0, 1023, 255, 0);
   analogWrite(led1, cor1);
-  analogWrite(led2, cor2);
+  analogWrite(led2, cor1);
+  analogWrite(led3, cor2);
+  analogWrite(led4, cor2);
 }
